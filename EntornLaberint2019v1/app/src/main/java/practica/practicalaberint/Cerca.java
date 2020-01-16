b6524a6db739a7cf32402a4809cf79c9232149ca
PoIIuX5 <tomas.bordoy@gmail.com>
2020-01-16 22:05:23 +0100

    Version Final

--- EntornLaberint2019v1/app/src/main/java/practica/practicalaberint/Cerca.java
+++ EntornLaberint2019v1/app/src/main/java/practica/practicalaberint/Cerca.java
@@ -13,15 +13,15 @@ import java.util.ListIterator;
 /**
  * AUTORS: Gian Lucas Martín Chamorro y Tomás Bordoy García-Carpintero
  */
-/*
- * S'ha d'emplenar la següent taula amb els diferents valors del nodes visitats
- * i llargada del camí resultat per les diferents grandàries de laberints
- * proposades i comentar breument els resultats obtinguts.
+/* S'ha d'emplenar la següent taula amb els diferents valors del nodes visitats i llargada del camí resultat
+ * per les diferents grandàries de laberints proposades i comentar breument els resultats obtinguts.
  ****************************************************************************************************************
- * Profunditat Amplada Manhattan Euclidiana Viatjant * Laberint Nodes Llargada
- * Nodes Llargada Nodes Llargada Nodes Llargada Nodes Llargada *
- * *****************************************************************************
- * ********************************* Petit Mitjà Gran
+ *                  Profunditat           Amplada          Manhattan         Euclidiana         Viatjant        *
+ *  Laberint     Nodes   Llargada    Nodes   Llargada   Nodes   Llargada   Nodes   Llargada  Nodes   Llargada   *
+ * **************************************************************************************************************
+ *    Petit
+ *    Mitjà
+ *    Gran
  *
  * Comentari sobre els resultats obtinguts:
  *
@@ -35,6 +35,7 @@ import java.util.ListIterator;
 public class Cerca {
 	Laberint laberint; // laberint on es cerca
 	int files, columnes; // files i columnes del laberint
+	Cami camiMin;          //
 
 	public Cerca(Laberint l) {
 		files = l.nFiles;
@@ -45,22 +46,22 @@ public class Cerca {
 	public Cami fesCerca(int tipus, Punt origen, Punt desti) {
 		if (esValid(origen) && esValid(desti)) {
 			switch (tipus) {
-			case (MainGame.AMPLADA):
-				return CercaEnAmplada(origen, desti);
+				case (MainGame.AMPLADA):
+					return CercaEnAmplada(origen, desti);
 
-			case (MainGame.PROFUNDITAT):
-				return CercaEnProfunditat(origen, desti);
+				case (MainGame.PROFUNDITAT):
+					return CercaEnProfunditat(origen, desti);
 
-			case (MainGame.MANHATTAN):
-			case (MainGame.EUCLIDEA):
-				return CercaAmbHeurística(origen, desti, tipus);
+				case (MainGame.MANHATTAN):
+				case (MainGame.EUCLIDEA):
+					return CercaAmbHeurística(origen, desti, tipus);
 
-			case (MainGame.VIATJANT):
-				laberint.posaCornalonsColor();
-				// es pot agafar la posició del cornaló i
-				// amb la funció "getObjecte(i)"
+				case (MainGame.VIATJANT):
+					laberint.posaCornalonsColor();
+					// es pot agafar la posició del cornaló i
+					// amb la funció "getObjecte(i)"
 
-				return CercaViatjant(origen, desti);
+					return CercaViatjant(origen, desti);
 			}
 		}
 		return null;
@@ -191,135 +192,130 @@ public class Cerca {
 			// generar succesors
 			ArrayList<Punt> succesors = veinats(actual);
 			for (Punt p : succesors) {
-				if (!tancat.contains(p)) {
-					double f, g, h;
-					g = actual.g + 1;
-					switch (tipus) {
-					case MainGame.MANHATTAN:
-						h = p.distanciaManhattan(desti);
-						break;
-					default:
-						h = p.distancia(desti);
-						break;
-					}
-					f = g + h;
-					if (p.f == 0 || f < p.f) {
-						p.f = f;
-						p.g = g;
-						p.h = h;
-						p.previ = actual;
+				double f, g, h;
+				g = actual.g + 1;
+				switch (tipus) {
+				case MainGame.MANHATTAN:
+					h = p.distanciaManhattan(desti);
+					break;
+				default:
+					h = p.distancia(desti);
+					break;
+				}
+				f = g + h;
+				if (p.f == 0 || f < p.f) {
+					p.f = f;
+					p.g = g;
+					p.h = h;
+					p.previ = actual;
+					tancat.remove(p);
+					if(!obert.contains(p)){
 						obert.add(p);
 					}
 				}
 			}
 		}
-		return cercarCami(trobat, camiTrobat,actual);
+		return cercarCami(trobat, camiTrobat, actual);
 	}
 
 	public Cami CercaViatjant(Punt origen, Punt desti) {
-        Cami camiTrobat = new Cami(files * columnes);
-        Cami camiActual = new Cami(files * columnes);
-        Cami camiMesCurt = new Cami(files * columnes);
-        Cami iterador;
+		Cami camiTrobat = new Cami(files * columnes);
+		Cami camiActual = new Cami(files * columnes);
+		Cami camiMesCurt = new Cami(files * columnes);
+		Cami iterador;
 
 
-        laberint.setNodes(0);
-        int p[] ={0,1,2,3};
-        int nKeys = 4;
-        int nodesVisitats = 0;
-        int bestTour = 0;
-        int longIterador = 0;
-        int longTotal[] = new int[4];
-        int longMin = 0, longCam = 0;
+		laberint.setNodes(0);
+		int p[] ={0,1,2,3};
+		int nKeys = 4;
+		int nodesVisitats = 0;
+		int bestTour = 0;
+		int longIterador = 0;
+		int longTotal[] = new int[4];
+		int longMin = 0, longCam = 0;
 
 
+		permuteHelper(origen, desti, p, 0);
 
 
-
-        return CrearPath(origen, desti, p);
-    }
-
-    public void permuteHelper( Punt origen, Punt desti, int[] arr, int index){
-        if(index >= arr.length - 1){ //If we are at the last element - nothing left to permute
-            //System.out.println(Arrays.toString(arr));
-            //Print the array
-            return;
-        }
-
-        for(int i = index; i < arr.length; i++){ //For each index in the sub array arr[index...end]
-
-            //Swap the elements at indices index and i
-            int t = arr[index];
-            arr[index] = arr[i];
-            arr[i] = t;
-
-            //Recurse on the sub array arr[index+1...end]
-            permuteHelper(origen, desti, arr, index+1);
-
-            //Swap the elements back
-            t = arr[index];
-            arr[index] = arr[i];
-            arr[i] = t;
-        }
-    }
+		return camiMin;
+	}
 
 
+	/******************** Funcions auxiliars ********************/
 
+	private Cami cercarCami(boolean trobat,Cami camiTrobat,Punt actual){
+		if (!trobat) {
+			System.err.println("ERROR: No s'ha trobat el camí");
+			return null;
+		}
+		do {
+			camiTrobat.afegeix(new Punt(actual));
+			actual = actual.previ;
+		} while (actual != null);
+		return camiTrobat;
+	}	Cami cami;
 
+	public Cami addPath (Cami camiActual, Punt origen, Punt desti){
+		Cami iterador;
 
+		iterador = CercaAmbHeurística(origen, desti, MainGame.MANHATTAN);
+		iterador.inverteix();
+		for (int x = 0; x < iterador.longitud; x++) {
+			camiActual.afegeix(iterador.cami[x]);
+		}
+		camiActual.nodesVisitats += iterador.nodesVisitats;
+		return camiActual;
+	}
 
-
-
-  /*  public static void permute(int start, int[] input) {
-        if (start == input.length) {
-            //System.out.println(input);
-            for (int x : input) {
-                System.out.print(x);
-            }
-            System.out.println("");
-            return;
-        }}
-*/
-        /******************** Funcions auxiliars ********************/
-
-		private Cami cercarCami(boolean trobat,Cami camiTrobat,Punt actual){
-			if (!trobat) {
-				System.err.println("ERROR: No s'ha trobat el camí");
-				return null;
+	public void permuteHelper(Punt origen, Punt desti, int[] arr, int index) {
+		if (index >= arr.length - 1) { // If we are at the last element - nothing left to permute
+			Cami c = createPath(origen, desti, arr);
+			if (c.longitud < camiMin.longitud) {
+				c.inverteix();
+				for (int x = 0; x < c.longitud; x++) {
+					camiMin.afegeix(c.cami[x]);
+				}
+				camiMin.nodesVisitats = c.nodesVisitats;
 			}
-			do {
-				camiTrobat.afegeix(new Punt(actual));
-				actual = actual.previ;
-			} while (actual != null);
-			return camiTrobat;
-		}	
 
-        public Cami addPath (Cami camiActual, Punt origen, Punt desti){
-              Cami iterador;
+			return;
+		}
 
-            iterador = CercaAmbHeurística(origen, desti, MainGame.MANHATTAN);
-            iterador.inverteix();
-            for (int x = 0; x < iterador.longitud; x++) {
-                camiActual.afegeix(iterador.cami[x]);
-            }
-            camiActual.nodesVisitats = iterador.nodesVisitats;
-            return camiActual;
-        }
+		for (int i = index; i < arr.length; i++) { // For each index in the sub array arr[index...end]
+
+			// Swap the elements at indices index and i
+			int t = arr[index];
+			arr[index] = arr[i];
+			arr[i] = t;
+
+			// Recurse on the sub array arr[index+1...end]
+
+			permuteHelper(origen, desti, arr, index + 1);
+
+			// Swap the elements back
+			t = arr[index];
+			arr[index] = arr[i];
+			arr[i] = t;
+		}
+	}
 
 
-        public Cami CrearPath (Punt origen, Punt desti, int [] Permutaciones){
-            Cami camiBase = new Cami(files * columnes);
 
-            camiBase = addPath(camiBase, origen, laberint.getObjecte(Permutaciones[0]));
-            camiBase = addPath(camiBase, laberint.getObjecte(Permutaciones[0]), laberint.getObjecte(Permutaciones[1]));
-            camiBase = addPath(camiBase, laberint.getObjecte(Permutaciones[1]), laberint.getObjecte(Permutaciones[2]));
-            camiBase = addPath(camiBase, laberint.getObjecte(Permutaciones[2]), laberint.getObjecte(Permutaciones[3]));
-            camiBase = addPath(camiBase, laberint.getObjecte(Permutaciones[3]), desti);
 
-            camiBase.inverteix();
+	public Cami createPath (Punt origen, Punt desti, int [] Permutaciones){
+		Cami camiBase = new Cami(files * columnes);
 
-            return camiBase;
+		camiBase = addPath(camiBase, origen, laberint.getObjecte(Permutaciones[0]));
+		camiBase = addPath(camiBase, laberint.getObjecte(Permutaciones[0]), laberint.getObjecte(Permutaciones[1]));
+		camiBase = addPath(camiBase, laberint.getObjecte(Permutaciones[1]), laberint.getObjecte(Permutaciones[2]));
+		camiBase = addPath(camiBase, laberint.getObjecte(Permutaciones[2]), laberint.getObjecte(Permutaciones[3]));
+		camiBase = addPath(camiBase, laberint.getObjecte(Permutaciones[3]), desti);
 
-        }
+		camiBase.inverteix();
+
+		return camiBase;
+
+	}
 
 }
